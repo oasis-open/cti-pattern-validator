6440736b1799f6282bb2797c783896941728c367
Greg Back <gback@mitre.org>
2016-12-06 12:14:54 -0600

    Add Trove classifiers.

--- setup.py
+++ setup.py
@@ -18,4 +18,14 @@ setup(
             'validate-patterns = stix2patterns.validator:main',
         ],
     },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
