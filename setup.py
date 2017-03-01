d144fea6bf9d570923c0f4c7d1ea7ae2cbd1b927
clenk <clenk@mitre.org>
2017-03-01 13:37:50 -0500

    Install only the correct antlr version, not both, in tox

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='stix2-patterns',
-    version='0.2.0',
+    version='0.2.2',
     packages=find_packages(),
     description='Validate STIX 2 Patterns.',
     install_requires=[
-        "antlr4-python2-runtime==4.5.3 ; python_version < '3'",
-        "antlr4-python3-runtime==4.5.3 ; python_version >= '3'",
-        "enum34 ; python_version ~= '3.3.0'",
         "six",
     ],
+    extras_require={
+        ':python_version<"3"': ['antlr4-python2-runtime==4.5.3'],
+        ':python_version>="3"': ['antlr4-python3-runtime==4.5.3'],
+        ':python_version~="3.3.0"': ['enum34'],
+    },
     entry_points={
         'console_scripts': [
             'validate-patterns = stix2patterns.validator:main',
